0e46b7a523f03c264b0ffae7889170a6d2022a91
Charles Jonathan Yingst <33520308+serviceNode1@users.noreply.github.com>
2024-08-27 18:47:12 -0400

    update to deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-# Get the current branch name
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-# Set the base directory
-BASE_DIR="/home/master/applications/CrowTours/public_html"
-
-# Set deployment directory based on branch
-if [ "$BRANCH" = "main" ]; then
-    DEPLOY_DIR="$BASE_DIR"
-elif [ "$BRANCH" = "master" ]; then
-    DEPLOY_DIR="$BASE_DIR/dev"
-else
-    echo "Unknown branch: $BRANCH"
-    exit 1
-fi
-
 # Navigate to the project directory
-cd $DEPLOY_DIR
+cd /home/master/applications/CrowTours/public_html
 
 # Install dependencies
 npm install
@@ -25,12 +9,12 @@ npm install
 # Build the React app
 npm run build
 
-# Move build files to the deployment directory
+# Move build files to the public_html directory
 mv build/* .
-mv build/.* . 2>/dev/null  # Suppress errors if no hidden files
+mv build/.* . 2>/dev/null
 rmdir build
 
-# Optionally, remove development files to save space
+# Optionally, remove development files
 rm -rf src public
 
-echo "Deployed $BRANCH branch to $DEPLOY_DIR"
+echo "Deployment completed"
