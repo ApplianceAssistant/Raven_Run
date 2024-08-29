f9ec9baa36623af2800c56cf5504945702a9b663
Charles Jonathan Yingst <33520308+serviceNode1@users.noreply.github.com>
2024-08-28 21:42:44 -0400

    updates to remaining pages and styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,16 @@
-// Contact.js
 import React from 'react';
 
 function Contact() {
-  return <div className="main-container">
-  <div className="home content">
-    <h1>Have a Question?</h1>
-  </div>
-</div>
+  return (
+    <div className="content-wrapper">
+      <div className="oval-content">
+        <div className="contact-content">
+          <h1>Have a Question?</h1>
+          <p>Feel free to reach out to us with any inquiries.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import '../css/Lobby.scss';
 
 function Lobby() {
   return (
-    <div className="lobby content">
-      <h2>Welcome to the Lobby</h2>
-      <p>Here you can join or create games.</p>
-      {/* Add more lobby content here */}
+    <div className="content-wrapper">
+      <div className="oval-content">
+        <div className="lobby-content">
+          <h2>Welcome to the Lobby</h2>
+          <p>Here you can join or create games.</p>
+          {/* Add more lobby content here */}
+        </div>
+      </div>
     </div>
   );
 }
