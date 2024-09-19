99ac6c7569791ac54e1b815b5327eeda85350971
Charles Jonathan Yingst <33520308+serviceNode1@users.noreply.github.com>
2024-09-19 09:10:54 -0400

    connection update

--- src/utils/dbProxy.js
+++ src/utils/dbProxy.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+const axios = require('axios');
 
 export async function dbQuery(query, params) {
   const response = await axios.post('/api/db-query', { query, params });
