e53847a6e6af314e857b24943039f691c66ea77e
Charles Jonathan Yingst <33520308+serviceNode1@users.noreply.github.com>
2024-09-23 17:38:57 -0400

    fixed light mode button text and added log in page

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -4,13 +4,10 @@ import { AuthContext } from '../App';
 import { checkServerConnectivity, API_URL } from '../utils/utils.js';
 import Modal from './Modal';
 
-function CreateAccount() {
+function LogIn() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isUsernameUnique, setIsUsernameUnique] = useState(true);
-  const [isEmailUnique, setIsEmailUnique] = useState(true);
-  const [isPasswordValid, setIsPasswordValid] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
@@ -31,70 +28,6 @@ function CreateAccount() {
       setServerStatus(status);
     });
   }, []);
-
-  const checkUnique = async (field, value) => {
-    try {
-      const response = await fetch(`${API_URL}/users.php?action=check_unique&field=${field}&value=${value}`);
-      const data = await response.json();
-      return data.isUnique;
-    } catch (error) {
-      console.error(`Error checking ${field} uniqueness:`, error);
-      return false;
-    }
-  };
-
-  const debounce = (func, delay) => {
-    let timeoutId;
-    return (...args) => {
-      clearTimeout(timeoutId);
-      timeoutId = setTimeout(() => func(...args), delay);
-    };
-  };
-
-  const debouncedUsernameCheck = debounce(async (value) => {
-    if (value) {
-      const isUnique = await checkUnique('username', value);
-      setIsUsernameUnique(isUnique);
-      if (!isUnique) {
-        setErrorMessage(`${value} is already taken, please choose a different username.`);
-      } else {
-        setErrorMessage('');
-      }
-    }
-  }, 300);
-
-  const debouncedEmailCheck = debounce(async (value) => {
-    if (value) {
-      const isUnique = await checkUnique('email', value);
-      setIsEmailUnique(isUnique);
-      if (!isUnique) {
-        setErrorMessage('This email is already associated with an account.');
-      } else {
-        setErrorMessage('');
-      }
-    }
-  }, 300);
-
-  const validatePassword = (value) => {
-    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\w\d!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]{8,}$/;
-    return regex.test(value);
-  };
-
-  useEffect(() => {
-    debouncedUsernameCheck(username);
-  }, [username]);
-
-  useEffect(() => {
-    debouncedEmailCheck(email);
-  }, [email]);
-
-  useEffect(() => {
-    setIsPasswordValid(validatePassword(password));
-  }, [password]);
-
-  const isFormValid = () => {
-    return username && email && isPasswordValid && isUsernameUnique && isEmailUnique;
-  };
 
   const handleSubmit = async (action) => {
     console.warn("handleSubmit", action);
@@ -157,7 +90,7 @@ function CreateAccount() {
   return (
     <div className="content-wrapper">
       <div className="bodyContent center">
-        <h1>Welcome, Brave Adventurer</h1>
+        <h1>Welcome Back, Adventurer</h1>
         {!serverStatus.isConnected && (
           <p className="error-message">Server is not connected. Please try again later.</p>
         )}
@@ -175,10 +108,8 @@ function CreateAccount() {
               id="username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              onBlur={(e) => debouncedUsernameCheck(e.target.value)}
               required
             />
-            {!isUsernameUnique && <p className="error-message">Username is already taken</p>}
           </div>
           <div className="account-field">
             <label htmlFor="email">Email:</label>
@@ -187,10 +118,8 @@ function CreateAccount() {
               id="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              onBlur={(e) => debouncedEmailCheck(e.target.value)}
               required
             />
-            {!isEmailUnique && <p className="error-message">Email is already in use</p>}
           </div>
           <div className="account-field">
             <label htmlFor="password">Password:</label>
@@ -201,19 +130,14 @@ function CreateAccount() {
               onChange={(e) => setPassword(e.target.value)}
               required
             />
-            {!isPasswordValid && password && (
-              <p className="error-message">
-                Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one number.
-              </p>
-            )}
           </div>
           <div className="button-container">
             <button 
-              onClick={() => handleSubmit('create')} 
-              disabled={!isFormValid() || isLoading}
-              className={`submit-button ${!isFormValid() || isLoading ? 'disabled' : ''}`}
+              onClick={() => handleSubmit('login')} 
+              disabled={!username || !password || isLoading}
+              className={`submit-button ${!username || !password || isLoading ? 'disabled' : ''}`}
             >
-              Create Account
+              Log In
             </button>
           </div>
         </form>
@@ -241,4 +165,4 @@ function CreateAccount() {
   );
 }
 
-export default CreateAccount;
+export default LogIn;
