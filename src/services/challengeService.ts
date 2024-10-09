903859530b913d6609ee9837a331b0d613dda0ed
Charles Jonathan Yingst <33520308+serviceNode1@users.noreply.github.com>
2024-10-09 18:36:47 -0400

    mode debug

--- src/services/challengeService.ts
+++ src/services/challengeService.ts
@@ -41,11 +41,11 @@ export function updateDistance(challenge: Challenge): {
 } {
   
   const userLocation = getCurrentLocation();
-  
+  alert("userLocation: " + userLocation + " challenge.targetLocation: " + challenge.targetLocation);
   if (userLocation && hasTargetLocation(challenge)) {
     const { distance, displayValue, unit } = calculateDistanceInfo(userLocation, challenge.targetLocation!);
     const direction = calculateDirection(userLocation, challenge.targetLocation!);
-    alert("update distance - distance: " + distance + " displayValue: " + displayValue + " unit: " + unit + " direction: " + direction);
+    alert("update distance - distance: " + distance);
     return { distance, displayValue, unit, direction };
   }
   alert("update distance - no user location or target location");
