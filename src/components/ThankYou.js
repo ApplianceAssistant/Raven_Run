58b6da8577eb49b3b986d9efb33125f12d1c951c
Charles Jonathan Yingst <33520308+serviceNode1@users.noreply.github.com>
2024-10-16 11:53:35 -0400

    centered headers

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -8,17 +8,19 @@ function ThankYou() {
                 <div className="content">
                     <ScrollableContent maxHeight="80vh">
                         <div className="base-container">
-                            <h1>Thank You for Joining the CrowTours Flock!</h1>
+                            <h1 className="center">Thank You for Joining the CrowTours Flock!</h1>
                             <p>We're thrilled to have you as part of our community of explorers and adventure-seekers.</p>
-                            <h2>Your Support Means the World</h2>
+
+                            <h2 className="center">Your Support Means the World</h2>
                             <p>Your subscription helps us create more exciting quests, uncover hidden gems, and bring adventure to every corner of the map. We're excited about the journey ahead and can't wait to share it with you.</p>
 
-                            <h2>What's Next?</h2>
+                            <h2 className="center">What's Next?</h2>
                             <ul>
                                 <li>What would you like to see next? Let us know!</li>
                                 <li>Want to create your own Scavengerhunts? We are working on a "Create your Own Hunt". Design your own adventure and invite friends to join the fun.</li>
                             </ul>
-                            <h2>Stay Connected</h2>
+                            
+                            <h2 className="center">Stay Connected</h2>
                             <ul>
                                 <li>Follow us on social media @CrowTours</li>
                                 <li>Share your experiences using #CrowTours</li>
