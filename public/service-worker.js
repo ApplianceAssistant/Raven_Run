1062517b73cdb73a1ad06565b07de0f0fcb50450
Charles Jonathan Yingst <33520308+serviceNode1@users.noreply.github.com>
2025-02-16 10:49:50 -0500

    update to serviceworker to enure newest version of site is served to visitors.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,61 @@
+const CACHE_NAME = 'crow-tours-v0.12';
+const CACHE_URLS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/static/js/main.js',
+  '/static/css/main.css',
+  '/images/icons/CrowTours_logo-192X192.png'
+];
+
+// Install event - cache initial resources
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(CACHE_URLS))
+      .then(() => self.skipWaiting()) // Ensure new service worker takes over immediately
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim()) // Take control of all clients immediately
+  );
+});
+
+// Fetch event - network first, fallback to cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Clone the response since we need to use it twice
+        const responseToCache = response.clone();
+        
+        // Update cache with new response
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+
+        return response;
+      })
+      .catch(() => {
+        // If network fails, try to get from cache
+        return caches.match(event.request);
+      })
+  );
+});
+
+// Push notification handler
 self.addEventListener('push', function(event) {
     const data = event.data.json();
     const options = {
@@ -12,4 +70,4 @@ self.addEventListener('push', function(event) {
     event.waitUntil(
       self.registration.showNotification(data.title, options)
     );
-  });
+});
